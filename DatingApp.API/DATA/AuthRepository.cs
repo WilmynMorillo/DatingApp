f7d4788d32bf3d5dff2f6d925c5e1456204a6cc1
Wilmyn Morillo <wmorillo@catojisa.com>
2018-10-10 18:49:35 -0400

    Error en byte

--- DatingApp.API/DATA/AuthRepository.cs
+++ DatingApp.API/DATA/AuthRepository.cs
@@ -1,7 +1,30 @@
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
 namespace DatingApp.API.DATA
 {
-    public class AuthRepository
+    public class AuthRepository : IAuthRepository
     {
-        
+        private readonly DataContext _context;
+        public AuthRepository(DataContext _context)
+        {
+            this._context = context;
+
+        }
+        public Task<User> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<User> Register(User user, string password)
+        {
+            bye[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, passwordHash, passwordSalt)
+        }
+
+        public Task<User> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
