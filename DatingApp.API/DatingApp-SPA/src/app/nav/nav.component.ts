ea6c52e78a0be9f9f47875108dec89c85a0ad74a
Wilmyn Morillo <wmorillo@catojisa.com>
2018-11-20 18:26:08 -0400

    20/11/2018

--- DatingApp.API/DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp.API/DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 
 @Component({
@@ -11,16 +12,16 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
      this.authService.login(this.model).subscribe(next => {
-       console.log('Logged in successfully');
+       this.alertify.succces('Logged in successfully');
      }, error => {
-       console.log(error);
+       this.alertify.error(error);
      });
     }
      loggedIn() {
@@ -30,7 +31,7 @@ export class NavComponent implements OnInit {
 
      loggedOut() {
       localStorage.removeItem('token');
-     console.log('logged out');
+     this.alertify.message('logged out');
     }
 
   }
